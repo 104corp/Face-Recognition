324ce083ceda72912596bbf3ce2e389b6eefb3d2
jiajun.chou <jiajun.chou@104.com.tw>
2020-07-27 10:06:29 +0800

    Test OpenCV api

--- src/face-recognition.py
+++ src/face-recognition.py
@@ -1,11 +1,16 @@
 # %%
 import numpy as np
 import cv2
+import os
 
+# get folder path
+os.chdir('../')
+cwd = os.getcwd() + '\\'
+print('Current Folder Path' + cwd)
 # %%
-img = cv2.imread('cat.jpg')
-
-# %%
-cv2.imshow('window_name', img)
+img = cv2.imread(cwd + 'img\cat.jpg')
+cv2.imshow('TestOpenCv', img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+# %%
 # %%
